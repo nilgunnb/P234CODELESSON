6d7850e3589346efb5b4ff381a4b20ed11d50604
Nilgun N. babazada <nilgunnb@code.local>
2023-04-06 14:57:33 +0400

    uploaded

---
+++ 12345 reverse.cs
@@ -0,0 +1,10 @@
+﻿int number = 12345;
+int reversenumber = 0;
+while (number>0)
+{
+    int numlast = number % 10;
+    reversenumber = reversenumber*10 + numlast;
+    number/=10;
+
+}
+Console.WriteLine(reversenumber);
---
+++ CustomSubstring.cs
@@ -0,0 +1,32 @@
+﻿string Word = "Hello world";
+
+CustomSubstring(Word, 1, 7);
+
+
+Word = CustomSubstring(Word, 1, 7);
+
+
+Console.WriteLine(Word);
+
+
+string CustomSubstring(string data, int startIndex, int endIndex)
+{
+    string result = string.Empty;
+
+    try
+    {
+
+        for (int i = startIndex; i <= endIndex; i++)
+        {
+            result += data[i];
+        }
+
+        return result;
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("bu index yoxdur.");
+    }
+
+    return "";
+}

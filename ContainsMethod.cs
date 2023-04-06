3e043ab05d4a6c69f00e24ca43dd7de3f459f2fe
Nilgun N. babazada <nilgunnb@code.local>
2023-04-06 15:59:36 +0400

    uploaded

---
+++ ContainsMethod.cs
@@ -0,0 +1,21 @@
+﻿using System.Security;
+
+string FullName = "Ahad Taghiyev";
+
+Console.WriteLine(ContainsMethod(FullName, 'a')); 
+
+bool ContainsMethod( string name, char letter)
+{
+
+
+    for (int i = 0; i < name.Length; i++)
+    {
+        if (name[i] == letter)
+        {
+            return true;
+        }
+    }
+        
+        return false;
+    
+}

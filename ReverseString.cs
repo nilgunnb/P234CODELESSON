7350a90b5550e5c49b0324efc678df0dae10fa22
Nilgun N. babazada <nilgunnb@code.local>
2023-04-06 15:31:16 +0400

    uploaded

---
+++ ReverseString.cs
@@ -0,0 +1,14 @@
+﻿string Word = "Hello, World";
+ 
+Word= CustomReverse(Word);
+Console.WriteLine(Word);
+string CustomReverse( string data)
+{
+    string Result = string.Empty;
+
+    for (int i = data.Length - 1; i >= 0; i--)
+    {
+        Result+= Word[i];
+    }
+    return Result;
+}

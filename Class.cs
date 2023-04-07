1734b094bc6178be515d60dd9d79dd29e52e5b0d
Nilgun N. babazada <nilgunnb@code.local>
2023-04-07 17:49:23 +0400

    uploaded

---
+++ Class.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+Developer Developer = new Developer("Semed", "Abdullayev", 28, 200, "Junior");
+Developer Developer1 = new Developer("Camal", "Hacizade", 24, 170, "Junior");
+Developer Developer2 = new Developer("Melissa", "Brown", 19, 1000, "Middle");
+Developer Developer3 = new Developer("Anna", "Dark", 18, 2000, "Senior");
+
+Developer.GetFullInfo();
+Developer1.GetFullInfo();
+Developer2.GetFullInfo();
+Developer3.GetFullInfo();
+

f8bc14897602a65ebc9779ca42f3616e610759a1
Nilgun N. babazada <nilgunnb@code.local>
2023-04-10 15:35:59 +0400

    uploaded

---
+++ Developer.cs
@@ -0,0 +1,32 @@
+﻿ class Developer
+    {
+    public string Name;
+    public string Surname;
+    public int Age;
+    public int Salary;
+    public string Position;
+    
+
+    public void GetFullInfo()
+    {
+        Console.WriteLine($"{this.Name}, {this.Surname},{this.Age}, {this.Salary},{this.Position}");
+    }
+
+
+
+    public Developer(string Name, string Surname, int Age, int Salary, string Position)
+    {
+        this.Name = Name;
+        this.Surname = Surname;
+        this.Age = Age;
+        this.Salary = Salary;
+        this.Position = Position;
+
+    }
+    
+    }
+
+
+    
+
+    

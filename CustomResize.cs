44897012e6b386f4c6e20358fb896f9ac1b24923
Nilgun N. babazada <nilgunnb@code.local>
2023-04-11 17:49:08 +0400

    uploaded

---
+++ CustomResize.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+int[] Numbers = { 1, 2, 3 };
+
+CustomResize( ref Numbers, 10);
+
+Console.WriteLine(Numbers.Length);
+
+void CustomResize ( ref int[] Array, int NewLength )
+{
+    int [] NewArray = new int[ NewLength ];
+
+    for ( int i = 0; i < NewArray.Length; i++)
+    {
+        if (i==NewArray.Length)
+        {
+            break;
+        }
+
+        NewArray[i] = Array[i];
+        
+        Array = NewArray;
+    }
+}
